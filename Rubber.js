e6e20279f0e0fc3162640ce8da61754955a51163
Ahirkrishna9tha <76474667+Ahirkrishna9tha@users.noreply.github.com>
2021-06-17 15:36:54 +0530

    Add files via upload

---
+++ Rubber.js
@@ -0,0 +1,33 @@
+class Rubber{
+	constructor(x,y,r)
+	{
+		var options = {
+		'restitution':0.3,
+		'friction':5,
+		'density':1.0
+	 }
+	// assign options to the rubber ball
+		this.x=x;
+		this.y=y;
+		this.r=r
+		this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options)
+		World.add(world, this.body);
+
+	}
+	display()
+	{
+		var pos =this.body.position;
+     	
+		var angle=this.body.angle	
+		push()
+		translate(pos.x, pos.y);
+		rotate(angle);
+		ellipseMode(CENTER)
+		strokeWeight(4);
+fill("darkblue");
+ellipse(0,0, this.r, this.r);
+
+		pop()
+	}
+
+}
